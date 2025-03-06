8104eea71cf51994f567691c9986fe41e856c548
litlemcelvain661 <litlemcelvain661@gmail.com>
2025-03-06 15:25:08 +0000

    Add 1.ts

---
+++ src/1.ts
@@ -0,0 +1,10 @@
+  import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-githubschoolproject',
+  templateUrl: './githubschoolproject.component.html',
+  styleUrls: ['./githubschoolproject.component.css']
+})
+export class GithubschoolprojectComponent {
+  constructor() {}
+}
