c6b533aa0c6c32522dcc88d913e2c35054be5fa0
litlemcelvain661 <litlemcelvain661@gmail.com>
2025-03-23 18:06:07 +0000

    Add 18.ts

---
+++ src/18.ts
@@ -0,0 +1,7 @@
+function sumOfSquares(a: number, b: number): number {
+  return Math.pow(a + b, 2);
+}
+
+// Test cases
+console.log(sumOfSquares(1, 2)); // Output: 5
+console.log(sumOfSquares(-3, 7)); // Output: 68
