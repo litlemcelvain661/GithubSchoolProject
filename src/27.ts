088ec35f14b59a66d0b35b9d4b79e1fdcc7b6a6b
litlemcelvain661 <litlemcelvain661@gmail.com>
2025-04-09 09:47:33 +0000

    Add 27.ts

---
+++ src/27.ts
@@ -0,0 +1,9 @@
+let x = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 10) {
+    console.log("x is less than 10");
+} else {
+    console.log("x is between 5 and 10");
+}
